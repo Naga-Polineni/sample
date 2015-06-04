d82b0803e0b00e0dc326d3db44899e2edea76d7f
nagapolineni <poli.naga43@gmail.com>
2015-06-04 22:02:39 +0000

    create page updated

--- app/controllers/recepies_controller.rb
+++ app/controllers/recepies_controller.rb
@@ -8,5 +8,27 @@ class RecepiesController < ApplicationController
     @recepie = Recepie.find(params[:id])
   end
   
+  def new
+    @recepie = Recepie.new
+  end
+  
+  def create
+    @recepie = Recepie.new(request_params)
+    @recepie.chef = Chef.find(1)
+  
+    if @recepie.save
+      flash[:success] = "Recepie has been added"
+      redirect_to recepies_path
+    else 
+    render :new
+    end
+  
+  end
+  
+  private 
+    
+    def request_params
+      params.require(:recepie).permit(:name,:summary,:description)
+    end
   
 end
--- app/models/recepie.rb
+++ app/models/recepie.rb
@@ -1,6 +1,6 @@
 class Recepie < ActiveRecord::Base
   belongs_to :chef
-  validates :name,  presence: true, length: {minimum: 3 , maximum: 100 }
+  validates :name,  presence: true, length: {minimum: 3 , maximum: 100 } 
   validates :summary,:description,  presence: true, length: {minimum: 4 , maximum: 400 }
   validates :chef_id, presence: true
 end
